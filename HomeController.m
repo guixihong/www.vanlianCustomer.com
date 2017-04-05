b6822f1d65603557518118ea1eb44b419cec9597
guixihong <forever_guixihong@126.com>
2017-04-05 18:27:23 +0800

    Vvv

--- HomeController.m
+++ HomeController.m
@@ -4,8 +4,8 @@
 //
 //  Created by SU on 2017/4/5.
 //  Copyright © 2017年 VKLJ_SU. All rights reserved.
-//fsdfdsfdsf
-//dsfiolfujsdl
+//
+
 #import "HomeController.h"
 
 @interface HomeController ()
--- vanlianClientTests/vanlianClientTests.m
+++ vanlianClientTests/vanlianClientTests.m
@@ -4,7 +4,7 @@
 //
 //  Created by SU on 2017/4/5.
 //  Copyright © 2017年 VKLJ_SU. All rights reserved.
-//grfgfgfgfg
+//
 
 #import <XCTest/XCTest.h>
 
